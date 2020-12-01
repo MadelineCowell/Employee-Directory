7045198250c6c7197faa21345e2891067a7d741a
MadelineC <madeline.cowell@colorado.edu>
2020-12-01 14:12:41 -0700

    Built dummy data

---
+++ public/assets/data.js
@@ -0,0 +1,48 @@
+// dummy employee data
+
+export const employeeData =
+
+    [
+        {
+            "id": 1,
+            "name": "Clinton Hughes",
+            "phone": "(303)-654-8706",
+            "email": "chughes@company.com",
+            "dob": "02-18-1960"
+        },
+        {
+            "id": 2,
+            "name": "Lila Howell",
+            "phone": "(303)-654-6504",
+            "email": "lhowell@company.com",
+            "dob": "10-6-1973"
+        },
+        {
+            "id": 3,
+            "name": "Ana Howard",
+            "phone": "(303)-654-1637",
+            "email": "ahoward@company.com",
+            "dob": "05-30-1997"
+        },
+        {
+            "id": 4,
+            "name": "Susan Hart",
+            "phone": "(303)-654-0319",
+            "email": "shart@company.com",
+            "dob": "10-03-1963"
+        },
+        {
+            "id": 5,
+            "name": "Jacqueline Perez",
+            "phone": "(303)-654-7820",
+            "email": "jperez@company.com",
+            "dob": "06-04-1982"
+        },
+        {
+            "id": 6,
+            "name": "Billy Bryant",
+            "phone": "(303)-654-6123",
+            "email": "bbryant@company.com",
+            "dob": "01-23-1990"
+        }
+    ]
